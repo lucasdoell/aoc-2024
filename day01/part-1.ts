ece5053a05d69d6acd4824005b8d72c35e57561c
Lucas Doell <lucas@lux.dev>
2024-12-01 17:08:47 -0500

    Complete part 1

--- day01/part-1.ts
+++ day01/part-1.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+const input = await Bun.file("day01/data.txt").text();
+
+// separate each input into its own list
+const leftList: number[] = [];
+const rightList: number[] = [];
+
+input.split("\n").map((str) => {
+  leftList.push(parseInt(str));
+  rightList.push(parseInt(str.substring(str.indexOf("   "))));
+});
+
+let distance = 0;
+
+Array.from({ length: leftList.length }).forEach(() => {
+  const smallestLeft = leftList.sort((a, b) => b - a).pop()!;
+  const smallestRight = rightList.sort((a, b) => b - a).pop()!;
+
+  distance += Math.abs(smallestLeft - smallestRight);
+});
